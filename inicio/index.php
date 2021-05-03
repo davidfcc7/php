15e483eb97e6a47bda4e0d09ed85e53a9486877c
David Caicedo <davidfcc7@gmail.com>
2021-05-03 16:52:34 -0500

    intruccion a php con variables y operaciones aritmeticas

---
+++ inicio/index.php
@@ -0,0 +1,26 @@
+<?php
+ // para realizar comentario
+ # mas comentarios
+ // todas las declarciones deben finalizar con ";" para comenzar la siguiente linea
+ /**
+  * para comentar varias lineas
+  * -> para hacer varias lineas de comentarios
+  * debemos comentar el codigo que hagamos para dar a entender el codigo a futuros programadores
+ */
+
+// Asignacion
+
+$num = 9;
+$lang = [
+    'es' => 'español', // => se usa para arreglos asociativos
+    'en' => 'ingles'
+];
+
+// Aritmetica
+echo "suma: ".((int) 2 + (int) 2); //sumar dos numeros enteros
+echo "resta: ".((int) 2 - (int) 2); //resta dos numeros enteros
+echo "multiplica: ".(2*2); // multiplica dos valores
+echo "divide: ".(2/2); # divide dos valores
+echo "modulo: ".(3%2); # me arroja el residuo de una division
+echo "exponencial: ".(2**2); # me arroja el valor de un exponente
+?>
