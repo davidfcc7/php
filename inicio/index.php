73d54e8c87942a7b0431ae858c096f639381b0a0
David Caicedo <davidfcc7@gmail.com>
2021-05-04 16:27:10 -0500

    variables variables

--- inicio/index.php
+++ inicio/index.php
@@ -23,4 +23,31 @@ echo "multiplica: ".(2*2); // multiplica dos valores
 echo "divide: ".(2/2); # divide dos valores
 echo "modulo: ".(3%2); # me arroja el residuo de una division
 echo "exponencial: ".(2**2); # me arroja el valor de un exponente
+
+//comparacion
+// valor '9' == 9 //compara el valor del dato
+// valor '9' === // compara el valor del dato y tambien el tipo
+// diferencia != de valor
+// diferencia !== de valor y tipo
+
+// Variables variables
+$nombre = 'david';
+$david = 'estudiante';
+
+echo $nombre; // -> david
+echo $$nombre; // -> estudiante
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
