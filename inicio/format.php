36f07efb2bb19fb758b56a11b42dfe561ba73b35
David Caicedo <davidfcc7@gmail.com>
2021-05-04 17:17:42 -0500

    composer

---
+++ inicio/format.php
@@ -0,0 +1,10 @@
+<?php
+// trae el valor del json en psr-4
+namespace Text;
+
+class format{
+    public static function upperText($value){
+        return strtoupper($value);
+    }
+}
+?>
---
+++ inicio/helpers.php
@@ -0,0 +1,3 @@
+<?php
+//es el php que llamo desde files en json
+?>
