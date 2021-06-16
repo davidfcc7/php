f51cad13a9b4c3ee079fce1efe0caa762f8b1a2f
David Caicedo <davidfcc7@gmail.com>
2021-06-16 08:10:54 -0500

    sintaxis

--- introduccion/index.php
+++ introduccion/index.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-    <h1>pimera pagina en php</h1>
-</body>
-</html>
+<?php
+/**
+ * creacion de una clase
+ * en php
+ */
+    class Answer
+    {
+        protected $clients = []; // crear una variable en php
+        protected $GLOBALS = []; # otra forma de hacer comentarios
+    }
