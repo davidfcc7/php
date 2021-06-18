466ebd9b4642bc422f6362c0c80dde94634d69cc
David Caicedo <davidfcc7@gmail.com>
2021-06-18 09:59:07 -0500

    composer dump

---
+++ composer/src/format.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace text;
+
+class format{
+    public static function upperText($value){
+        return strtoupper($value);
+    }
+}
---
+++ composer/src/helpers.php
@@ -0,0 +1,5 @@
+<?php
+
+function upper ($value) {
+    return text\format::upperText($value);
+}
